1d1f60dc5dc16af07bbf9f7f24fdb6ac0ebbf020
nsandasika <83905059+nsandasika@users.noreply.github.com>
2023-04-06 03:16:27 +0530

    Update profile-controller.js

--- backend/backend/controllers/profile-controller.js
+++ backend/backend/controllers/profile-controller.js
@@ -1,26 +1,45 @@
+// Importing the user model from ../models/user-model file.
 const Usermodel = require("../models/user-model");
-
+//Importing the cloudinary object from ../utils/cloudinary file.
 const { cloudinary } = require("../utils/cloudinary");
-
+//Importing the mongoose module for database operations.
 const mongoose = require("mongoose");
 
 
 //fetch customer profile data
+//Exporting an asynchronous function as getProfileData with req and res parameters
 exports.getProfileData = async (req, res) => {
-  try {
+ 
+ //Starting a try-catch block to handle errors.
+ try {
+ 
+    //Checking if req.user is not defined
     if (!req.user) {
+    
+      //Setting response status code to 422 and returning a JSON object.
       res.status(422).json({
+      
+        //Setting the success status to false
         success: false,
+        
+        //Setting the description of the error.
         desc: "Can not find the user - Please check again",
       });
     } else {
+    
+      //Setting response status code to 200 and returning a JSON object.
       res.status(200).send({
+        //Adding the req.user object to the JSON object
         profile: req.user,
       });
     }
+    
+    //Catching errors if any occurred.
   } catch (error) {
     res.status(500).json({
       success: false,
+      
+      //Setting the description of the erro
       desc: "Error in getProfileData controller-" + error,
     });
   }
@@ -28,13 +47,22 @@ exports.getProfileData = async (req, res) => {
 
 
 //Fetch User profile details
+//Exporting an asynchronous function as getUserDetails with req and res parameters.
 exports.getUserDetails = async (req, res) => {
   try {
+  
+    //Finding all the user details using Usermodel and assigning it to the userdetails variable
     const userdetails = await Usermodel.find();
+    
+    //Setting response status code to 200 and returning a JSON object.
     res.status(200).send({
+    
+      //Adding the userdetails object to the JSON object
       userdetails,
     });
   } catch (error) {
+  
+    //Setting response status code to 500 and returning a JSON object.
     res.status(500).json({
       success: false,
       desc: "Error in getUser Details controller-" + error,
@@ -45,7 +73,10 @@ exports.getUserDetails = async (req, res) => {
 
 
 //delete user
+//Exporting an asynchronous function as deleteUser with req and res parameters
 exports.deleteUser = async (req, res) => {
+
+  //Getting the id parameter from the request.
   let Id = req.params.id;
 
   if (!mongoose.Types.ObjectId.isValid(Id))
@@ -62,17 +93,26 @@ exports.deleteUser = async (req, res) => {
 
 
 //edit user
+// exports a function called updateuser that takes in a req (request) and res (response) parameters
 exports.updateuser =  async (req,res) =>{
+
+  //destructures the password property from the req.body object and assigns it to a constant password
   const { password } = req.body;
 
   try {
+  //creates a new object called newData that has a single property password with the value of the password constant
     const newData = {
       password
     };
+    
+    
+    //finds a user in the database by their ID and updates their password using the findByIdAndUpdate method from Mongoose.
     const updatedUser = await Usermodel.findByIdAndUpdate(
       req.user.id,
       newData,
       {
+      
+        //options for the findByIdAndUpdate method
         new: true,
         upsert: false,
       }
@@ -82,7 +122,11 @@ exports.updateuser =  async (req,res) =>{
       desc: "user updated successfully",
       updatedUser,
     });
+    
+    //If an error occurs in the try block, the catch block is executed
   } catch (error) {
+  
+    //If an error occurs, the server responds with an HTTP status code of 500 and sends a JSON object with a success property set to false
     res.status(500).json({
       success: false,
       desc: "Error in updateProfileData controller-" + error,
