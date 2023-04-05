edebbb8025fb0162c21bc12b645cb2d2a5f329ed
nsandasika <83905059+nsandasika@users.noreply.github.com>
2023-04-06 03:08:38 +0530

    Update authentication-controller.js

--- backend/backend/controllers/authentication-controller.js
+++ backend/backend/controllers/authentication-controller.js
@@ -1,19 +1,23 @@
 
+// import the Usermodel and cloudinary dependencies from their respective files.
 const Usermodel =require("../models/user-model");
 const { cloudinary } = require("../utils/cloudinary");
 
 // login controller
+//exports a function called login as part of the exports object.
 exports.login = async (req, res, next) => {
+  //function retrieves the email and password values from the request body
   const { email, password } = req.body;
   //check user
   let user;
- 
+    //uses Usermodel to find a user with the specified email, and includes the password field in the returned object.
     user = await Usermodel.findOne({ email: email }).select("+password");
   
   //check password match
+  //etrieved user object and calls the matchPasswords method to check if the entered password matches the hashed password in the database.
   try {
     const isMatch = await user.matchPasswords(password);
-
+  //If it does match, the function calls sendToken with the user object, 200 HTTP status code, and res object
     if (!isMatch) {
       res.status(401).send({
         success: false,
@@ -29,17 +33,22 @@ exports.login = async (req, res, next) => {
 
 
 // register new user
+//exports a function called registerUser as part of the exports object.
+It takes in req and res as arguments.
 exports.registerUser = async (req, res) => {
+  //The function retrieves the email, password, username, role, and fileEnc values from the request body
   const {  email, password , username,role,fileEnc } = req.body;
 //check for users with same email address within customer collection
 let existingEmail = await findUserByEmail(email);
 if (existingEmail) {
   existingEmail = null;
+  //If it does exist, the function sends an error message with a 401 HTTP status code
   res.status(401).json({
     success: false,
     desc: "Email already exist - Please check again",
   });
 } else {
+  //If it doesn't exist, the function uploads the fileEnc to Cloudinary and saves the resulting URL and public ID as part of the profilePicture object.
     try {   
       const profileimage = await cloudinary.uploader.upload(fileEnc, {
         upload_preset: "ssd_assignment",
@@ -71,6 +80,8 @@ if (existingEmail) {
 //send response object to client if login success
 const sendToken = (user, statusCode, res) => {
   const token = user.getSignedToken();
+      desc: "Error occured in findUserByEmail segment",
+  // 
   res.status(statusCode).json({ sucess: true, token, user });
 };
 
@@ -79,6 +90,7 @@ const sendToken = (user, statusCode, res) => {
 const findUserByEmail = async (email) => {
   let existingAccount;
   try {
+    //It then creates a new user using Usermodel with the provided values, including the profilePicture object
     existingAccount = await Usermodel.findOne({ email: email });
     return existingAccount;
   } catch (err) {
